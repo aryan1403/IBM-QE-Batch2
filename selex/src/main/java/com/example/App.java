1590eea3c14b615af78558dfd3c51ea4396ffed1
aryan1403 <aaryan14032006@email.com>
2026-03-09 11:56:38 +0530

    waits locators

--- selex/src/main/java/com/example/App.java
+++ selex/src/main/java/com/example/App.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class App {
     getDriver driver = new getDriver();
+
+    public void waitFor(WebDriver driver, int time) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(time));
+    }
     public String getTitle() {
         WebDriver driver = this.driver.chromWebDriver();
 
@@ -16,19 +25,37 @@ public class App {
 
         return title;
     }
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
+    public void search() throws InterruptedException {
+        WebDriver driver = this.driver.chromWebDriver();
 
-        // Create WebDriver instance
-        WebDriver driver = new ChromeDriver();
-
-        // Navigate to website
         driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.id("APjFqb"));
+        searchBox.sendKeys("github aryan1403");
+        this.waitFor(driver, 2000);
 
-        // Print title to verify
-        System.out.println("Page title: " + driver.getTitle());
+        searchBox.submit();
 
-        // Close browser
+        String currentUrl = driver.getCurrentUrl().toString();
+        this.waitFor(driver, 1000);
+        System.out.println("Current URL: " + currentUrl);
         driver.quit();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        // WebDriverManager.chromedriver().setup();
+
+        // // Create WebDriver instance
+        // WebDriver driver = new ChromeDriver();
+
+        // // Navigate to website
+        // driver.get("https://www.google.com");
+
+        // // Print title to verify
+        // System.out.println("Page title: " + driver.getTitle());
+
+        // // Close browser
+        // driver.quit();
+
+        App app = new App();
+        app.search();
     }
 }
